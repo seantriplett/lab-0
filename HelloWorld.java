848d54e6190dd28e91f9b4ca1e998dbc9494ee52
seantriplett <156947409+seantriplett@users.noreply.github.com>
2024-01-17 14:04:17 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,27 @@
 //HelloWorld.java
-
+// Programmers:  Sean Triplett
+// Course:  CS 212, Dr. Cui
+// Due Date: 1/19/24
+// Lab Assignment: 0
+// Problem Statement:  Output programmers’ initial
+// Input: None; there is no user input in this program
+// Output:  The initials of the programmers in ASCII art form
+// Credits: N/A
 class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
-        
+        System.out.println("-----------      ------------------");
+        System.out.println("|                         |");
+        System.out.println("|                         |");
+        System.out.println("|                         |");
+        System.out.println("|                         |");
+        System.out.println("|                         |");
+        System.out.println("-----------               |");
+        System.out.println("          |               |");
+        System.out.println("          |               |");
+        System.out.println("          |               |");
+        System.out.println("          |               |");
+        System.out.println("          |               |");
+        System.out.println("-----------               |");
     }
 }
